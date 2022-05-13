7d9b53f4e17d786e48e770c3dcd1168b592abf89
lorenzosavoia <lorenzo.savoia97@gmail.com>
2022-05-13 15:20:11 +0200

    Aggiungere i file di progetto.

---
+++ CodeFile1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace test
+{
+    //Extension methods are defined as static methods but are called by
+    //using instance method syntax.Their first parameter specifies which
+    //type the method operates on.The parameter is preceded by the this
+    //modifier.Extension methods are only in scope when you explicitly
+    //import the namespace into your source code with a using directive.
+    public static class rova
+    {
+        public static int WordCount(this string inputString)
+        {
+            //La Split delle stringhe è un'istruzione molto potente per spezzettare una stringa in elementi più piccoli (sottostringhe)
+            return inputString.Split(new char[] { ' ', '.', ';' /* ... */ },
+                                     StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //args è un array/vettore di stringhe
+            //la linea di comando
+            //Eseguire con .\test parametri...
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Parametro {0}: {1}", i, args[i]);
+            }
+            //per provare a fare debug
+            int a = 10;
+            if (a > 20)
+            {
+                Console.WriteLine("Maggiore di venti");
+            }
+            else
+            {
+                Console.WriteLine("Minore o uguale a 20");
+            }
+            //le extention per estendere una classe "sealed"
+            Console.WriteLine("pippo pluto paperino".WordCount());
+            Console.WriteLine("Fine");
+        }
+    }
+}
